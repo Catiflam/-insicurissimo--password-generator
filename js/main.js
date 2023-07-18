fa754196a2de17ff66ea3721af04252a143330e7
Catiflam <gilfrid1@gmail.com>
2023-07-18 20:14:11 +0200

    edit track

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-const username = prompt(" Inserisci il tuo nome");
+// const username = prompt(" Inserisci il tuo nome");
+// const surname = prompt("inserisci il tuo cognome");
+// const favouritecolor = prompt("inserisci il colore preferito");
 
-console.log(username);
+const lorem = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, cum!";
+
+const welcomeMessage = `
+ Benvenuto, filippo
+ <p>${lorem}</p> `;
+
+// document.getElemen;tById("userinfo").innerHTML = welcomeMessage
+
+// console.log("inserisci l'insieme dei dati" + username + surname + favouritecolor = welcomeMessage);
